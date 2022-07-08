377764631d64b277454a2042f22ebeaa924b46ab
ymelnykov <ymelnykov@users.noreply.github.com>
2022-07-08 13:17:31 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -4,24 +4,23 @@
 
 int main(void)
 {
-    long num;
-    do
-    {
-        num = get_long("Number: ");
-    }
-    while (num % (long) pow(10, 12) == num || num % (long) pow(10, 16) != num);
+    long num = get_long("Number: ");
 
 //Calculating the checksum as per Luhn’s Algorithm
 
     int sum = 0;
     int digit = 0;
+    int digit_2 = 0;
+    int digits_no = 0;
     for (int i = 1; i <= 16; i++)
     {
         if (num % (long) pow(10, i-1) == num)
         {
             break;
         }
-        digit = floor(num % (long) pow(10, i) / pow(10, (i-1)));
+        digit = floor(num % (long) pow(10, i) / pow(10, i-1));
+        digit_2 = floor(num % (long) pow(10, i-1) / pow(10, i-2));
+        digits_no = i;
         if (i % 2 == 0)
         {
             sum = (digit * 2 > 9) ? sum + ((digit * 2) % 10 + floor (digit * 2 / 10)) : sum + digit * 2;
@@ -31,24 +30,27 @@ int main(void)
             sum += digit;
         }
     }
+    int digits = digit * 10 + digit_2;
 
 //Checking the card number
 
     if (sum % 10 == 0)
     {
-        switch(digit)
+        if ((digits == 34 || digits == 37) && digits_no == 15)
         {
-            case 3:
-                printf("AMEX\n");
-                break;
-            case 4:
-                printf("VISA\n");
-                break;
-            case 5:
-                printf("MASTERCARD\n");
-                break;
-            default:
-                printf("INVALID\n");
+            printf("AMEX\n");
+        }
+        else if (digit == 4 && (digits_no == 13 || digits_no == 16))
+        {
+            printf("VISA\n");
+        }
+        else if ((digits == 51 || digits == 52 || digits == 53 || digits == 54 || digits == 55) && digits_no == 16)
+        {
+            printf("MASTERCARD\n");
+        }
+        else
+        {
+            printf("INVALID\n");
         }
     }
     else
