f05fb0e143f5bd0ecb46dadeaf067580737a8e0d
ymelnykov <ymelnykov@users.noreply.github.com>
2022-07-16 12:07:45 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -8,14 +8,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int blue = image[i][j].rgbtBlue;
-            int green = image[i][j].rgbtGreen;
-            int red = image[i][j].rgbtRed;
-            int average = round ((blue + green + red) / 3);
-            image[i][j].rgbtBlue = average;
-            image[i][j].rgbtGreen = average;
-            image[i][j].rgbtRed = average;
-
+            int rgbtAV = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen +image[i][j].rgbtRed) / 3.0);
+            image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed = rgbtAV;
         }
     }
     return;
